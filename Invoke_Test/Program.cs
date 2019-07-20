bc1a59a68ed6bffa4c662c746a572a5b8c393eba
NemesLaszlo <wow.laszlo@gmail.com>
2019-07-20 15:31:45 +0200

    Working Dummy

---
+++ Invoke_Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Invoke_Test2;
+
+namespace Invoke_Test
+{
+  
+
+ 
+
+    public partial class Testing
+    {
+        public void Test()
+        {
+
+            //var valami = AppDomain.CurrentDomain.GetAssemblies()[15];
+            
+            List<testobj> testlist = new List<testobj>();
+            testlist.Add(new testobj("elso"));
+            testlist.Add(new testobj("masodik"));
+            //var d = Activator.CreateInstance(typeof(TestInvokeClass),true) as TestInvokeClass;
+           
+
+            foreach (var item in testlist)
+            {
+                
+                var args = new object[] { item };
+                if(args != null)
+                {
+                   //d.GetType().GetMethod("Writesomething", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(d,args);
+                   typeof(TestInvokeClass).GetMethod("Writesomething", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new Invoke_Test2.TestInvokeClass(), args);
+                }
+                
+                
+                
+                
+            }
+        }
+}
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Testing test = new Testing();
+            test.Test();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Invoke_Test2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoke_Test2
+{
+    public class testobj
+    {
+        public string name { get; set; }
+        public testobj(string name)
+        {
+            this.name = name;
+        }
+    }
+    internal abstract class Testclass
+    {
+
+    }
+
+    class TestInvokeClass : Testclass
+    {
+        private void Writesomething(testobj obj)
+        {
+            if (obj.name.Equals("elso"))
+            {
+                Console.WriteLine("Működik");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+}
